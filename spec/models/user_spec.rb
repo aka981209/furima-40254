db97645dc95a9d9e32c9312cdb9d9975182e1990
aka981209 <tw981209@gmail.com>
2024-02-02 14:52:56 +0900

    fix02

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -118,7 +118,7 @@ RSpec.describe User, type: :model do
       it '生年月日が空では登録できない' do
         @user.birth_date = ''
         @user.valid?
-        expect(user.errors.full_messages).to include("Birth date can't be blank")
+        expect(@user.errors.full_messages).to include("Birth date can't be blank")
       end
     end
   end
